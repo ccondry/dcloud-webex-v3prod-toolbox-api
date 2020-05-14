30ba015cc496064f081ebf40fb073abea98ad23d
Coty Condry <ccondry@cisco.com>
2020-05-14 12:37:11 -0500

    fix reference to userId as "id"

--- src/routes/provision.js
+++ src/routes/provision.js
@@ -29,7 +29,7 @@ router.post('/', async function (req, res, next) {
 
     console.log('marking user', username, userId, 'as provisioned but not done for Webex v3')
     // mark user provisioned but not done in our cloud db
-    await model.set({username, userId, isDone: false})
+    await model.set({username, id: userId, isDone: false})
 
     console.log('sending CUCM provision request to Webex v3 demo session...')
     // forward the JWT to the CUCM inside the demo session
